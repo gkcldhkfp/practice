1689d4628afd27b136bcc164f2e9c55e5a0da025
dh, kim <kds6723@naver.com>
2024-03-26 18:57:27 +0900

    03-26

--- lab06_class/src/com/itwill/class05/ClassMain05.java
+++ lab06_class/src/com/itwill/class05/ClassMain05.java
@@ -4,9 +4,9 @@ public class ClassMain05 {
 
 	public static void main(String[] args) {
 
-		// Subject, Student 클래스 객체 생성과 메서드 호출 결과 테스트.
+		//  Subject, Student 클래스 객체 생성과 메서드 호출 결과 테스트.
 		Subject sub = new Subject();
-
+		
 		sub.english = 80;
 		sub.korean = 90;
 		sub.math = 60;
@@ -19,9 +19,9 @@ public class ClassMain05 {
 		System.out.println(stu.subject.math);
 
 		System.out.println("total = " + sub.total());
-
+		
 		System.out.println("mean = " + sub.mean());
-
+		
 		stu.info();
 
 	}
