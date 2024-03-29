8c9bc5bb0fc73213fbb842dd5e7154721a83f383
gkcldhkfp <164485373+gkcldhkfp@users.noreply.github.com>
2024-03-29 18:30:35 +0900

    Delete lab06_class directory

--- lab06_class/src/com/itwill/enum01/Season1.java
+++
@@ -1,8 +0,0 @@
-package com.itwill.enum01;
-
-public class Season1 {
-	public static final int SPRING = 0;
-	public static final int SUMMER = 1;
-	public static final int FALL = 2;
-	public static final int WINTER = 3;
-}
--- lab06_class/src/com/itwill/modifier05/PublicClassEx.java
+++
@@ -1,5 +0,0 @@
-package com.itwill.modifier05;
-
-public class PublicClassEx {
-
-}
